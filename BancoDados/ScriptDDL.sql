0987d1834a29c96ec36e4fcf9c0c66522d16d2a0
giovannagrigo <giovanna1grigo@gmail.com>
2025-02-06 14:50:37 -0300

    Finalização do Banco e criação do Template

--- BancoDados/ScriptDDL.sql
+++ BancoDados/ScriptDDL.sql
@@ -8,3 +8,23 @@ CREATE TABLE Categoria(
 	Foto		VARCHAR(200),
 	ExibirHome	BIT
 );
+
+CREATE TABLE Produto(
+	Id			INT not null IDENTITY PRIMARY KEY,
+	Nome		VARCHAR(100) not null,
+	Descricao	VARCHAR(1000),
+	CategoriaId	INT not null,
+	ValorCusto	NUMERIC(10,2),
+	ValorVenda	NUMERIC(10,2) not null,
+	QtdeEstoque INT not null DEFAULT 0,
+	Destaque	BIT DEFAULT 0,
+	FOREIGN KEY	(CategoriaId) REFERENCES Categoria(Id)
+);
+
+CREATE TABLE ProdutoFoto(
+	Id			INT not null IDENTITY PRIMARY KEY,
+	ProdutoId	INT not null,
+	Foto		VARCHAR(200) not null,
+	Descricao	VARCHAR(100)
+	FOREIGN KEY (ProdutoId) REFERENCES Produto(Id)
+);
