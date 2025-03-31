2fde7bcb547fbe2a2892b43bf2fe05b570da080a
giovannagrigo <giovanna1grigo@gmail.com>
2025-03-31 16:41:31 -0300

    Adiciona IdentityRoles

--- LojaGrigo/Data/AppDbSeed.cs
+++ LojaGrigo/Data/AppDbSeed.cs
@@ -1,4 +1,5 @@
 using LojaGrigo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace LojaGrigo.Data;
@@ -197,8 +198,72 @@ public class AppDbSeed
             new() {Id = 25, ProdutoId = 14, Foto = "/img/produtos/Polos/PoloViagemF.webp"},
             new() {Id = 26, ProdutoId = 14, Foto = "/img/produtos/Polos/PoloViagemT.webp"},
             new() {Id = 27, ProdutoId = 15, Foto = "/img/produtos/Polos/PoloCasualF.png"},
-            new() {Id = 28, ProdutoId = 15, Foto = "/img/produtos/Polos/PoloCasualT.webp"},
+            new() {Id = 28, ProdutoId = 15, Foto = "/img/produtos/Polos/PoloCasualT.webp"}
         };
         builder.Entity<ProdutoFoto>().HasData(fotos);
+
+        #region Populate Roles - Perfis de Usuário
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole() {
+                Id = "0b44ca04-f6b0-4a8f-a953-1f2330d30894",
+                Name = "Administrador",
+                NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole() {
+                Id = "bec71b05-8f3d-4849-88bb-0e8d518d2de8",
+                Name = "Funcionário",
+                NormalizedName = "FUNCIONÁRIO"
+            },
+            new IdentityRole() {
+                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+                Name = "Cliente",
+                NormalizedName = "CLIENTE"
+            },
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+        #endregion
+
+        #region Populate Usuário
+        List<Usuario> usuarios = new() {
+            new Usuario(){
+                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",          
+                Email = "giovanna1grigo@gmail.com",
+                NormalizedEmail = "GIOVANNA1GRIGO@GMAIL.COM",
+                UserName = "GihGrigo",
+                NormalizedUserName = "GIHGRIGO",
+                LockoutEnabled = true,
+                EmailConfirmed = true,
+                Nome = "Giovanna Grigolato",
+                DataNascimento = DateTime.Parse("14/12/2006"),
+                Foto = "/img/giovanna.webp"
+            }
+        };
+        foreach (var user in usuarios)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "123456");
+        }
+        builder.Entity<Usuario>().HasData(usuarios);
+        #endregion
+
+        #region Populate UserRole - Usuário com Perfil
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[0].Id
+            },
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[1].Id
+            },
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[2].Id
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
     }
 }
